cee17d29c96c9332c1a35f39ff370d680a01154f
Guilherme Saraiva <guilherme-saraiva@hotmail.com>
2025-01-22 05:29:41 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const descriptions = ["Web developer", "UX/UI designer", "Content creator"];
+const logoDescription = document.querySelector(".logo-description");
+let index = 0;
+
+function changeText() {
+    logoDescription.classList.remove("show-text"); // Remove a classe para iniciar a transição
+    setTimeout(() => {
+        logoDescription.textContent = descriptions[index]; // Atualiza o texto
+        logoDescription.classList.add("show-text"); // Adiciona a classe para mostrar o novo texto
+        index = (index + 1) % descriptions.length; // Vai para o próximo texto, e reinicia ao chegar no final
+    }, 500); // Atraso de 0.5s antes de trocar o texto (durante a transição)
+}
+
+setInterval(changeText, 6000); // Troca o texto a cada 3 segundos
+changeText(); // Chama uma vez para iniciar
