37dd5c5b1bddd3f46a1a1e98b4df592bb9df912a
Guilherme Saraiva <guilherme-saraiva@hotmail.com>
2025-02-05 02:45:19 -0300

    🐛 bug: correção do scroll ao retornar do menu

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
     menu.addEventListener("click", (e) => {
         if (e.target === menu) {
             menu.classList.remove("active");
+            document.body.classList.remove("no-scroll");
         }
     });
 });
